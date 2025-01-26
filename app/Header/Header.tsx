e507d07ccd2e8c089f30d328ccc99228ceb4204b
Eduard Stef <e.stef@netmatch.ro>
2025-01-26 19:24:55 +0200

    implemented login form component

--- app/Header/Header.tsx
+++ app/Header/Header.tsx
@@ -1,15 +1,26 @@
 'use client';
 import React from 'react';
-import Menu from './Menu';
 import Logo from './Logo';
+import { useRouter } from 'next/navigation';
 import '../styles/menu.scss';
 
 const Header = () => {
+	const router = useRouter();
+	const hoverLoginPage = () => {
+		router.push('/login-page');
+	};
+
 	return (
 		<div className="menu-navbar">
 			<Logo />
-			<Menu />
-			<p>LogIn</p>
+			<div className="menu-list">
+				<p>Home</p>
+				<p>Pages</p>
+				<p>Features</p>
+			</div>
+			<p onClick={hoverLoginPage}>
+				LogIn
+			</p>
 		</div>
 	);
 };
